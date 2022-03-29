d58fdd6da17c1a8181955616085baf40cda1b279
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-03-29 21:31:20 +0700

    fix keyword

--- cloudflareIP.js
+++ cloudflareIP.js
@@ -13,11 +13,11 @@ module.exports = class cloudflareIP {
         let processedIp = ipaddr.process(ip);
 
         if (processedIp instanceof ipaddr.IPv4) {
-            return range_check.inRange(processedIp.toString(), range.v4)
+            return range_check.inRange(processedIp.toString(), this.range.v4)
         }
 
         if (processedIp instanceof ipaddr.IPv6) {
-            return range_check.inRange(processedIp.toString(), range.v6)
+            return range_check.inRange(processedIp.toString(), this.range.v6)
         }
 
         return false;
